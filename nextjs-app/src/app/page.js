c49644e2eab2e2fd84edfac566c648f3182d5865
Liam Gilbert <gilbert.li@northeastern.edu>
2024-10-12 19:56:21 -0400

    Service page in React

--- nextjs-app/src/app/page.js
+++ nextjs-app/src/app/page.js
@@ -1,15 +1,17 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import Service from './service/page';
 
 const FramerEmbed = () => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <iframe
-        src="https://accelerate-estimates.framer.website/"
-        style={{ width: '100%', height: '100%', border: 'none' }}
-        title="Landing Page"
-      />
-    </div>
+    // <div style={{ width: '100%', height: '100vh' }}>
+    //   <iframe
+    //     src="https://accelerate-estimates.framer.website/"
+    //     style={{ width: '100%', height: '100%', border: 'none' }}
+    //     title="Landing Page"
+    //   />
+    // </div>
+    <Service/>
   );
 };
 
