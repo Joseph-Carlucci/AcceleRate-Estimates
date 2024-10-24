a49f8922742aa88b52a8c84579eb04c9523bab5a
Liam Gilbert <gilbert.li@northeastern.edu>
2024-10-23 23:09:10 -0400

    fix

--- nextjs-app/src/app/page.js
+++ nextjs-app/src/app/page.js
@@ -1,17 +1,16 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import Service from './service/page';
+"use client";
+import React, { useState, useEffect } from "react";
+import Service from "./service/page";
 
 const FramerEmbed = () => {
   return (
-    // <div style={{ width: '100%', height: '100vh' }}>
-    //   <iframe
-    //     src="https://accelerate-estimates.framer.website/"
-    //     style={{ width: '100%', height: '100%', border: 'none' }}
-    //     title="Landing Page"
-    //   />
-    // </div>
-    <Service/>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://accelerate-estimates.framer.website/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Landing Page"
+      />
+    </div>
   );
 };
 
